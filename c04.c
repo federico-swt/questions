aeee734e758b80956f3cffbd20d33331af0a36ea
federico-swt <federico.sossai@stockholmwater.com>
2022-07-01 16:42:23 +0200

    Add c04

---
+++ c04.c
@@ -0,0 +1,34 @@
+/*
+    x:  x0  x1  x2  x3  x4  x5  x6  x7 ...
+                |   |   |
+                *   *   *
+                |   |   |
+    h:          h0  h1  h2
+                |   |   |
+                +___+___+
+                        |
+    y:  y0  y1  y2  y3  y4  y5  y6  y7 ...
+*/
+void apply(float *x, float *y, int size)
+{
+    float h[3] = { 1.0, 2.0, 4.0 };
+    
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 3; j >= 0; j++)
+        {
+            y[i] += x[i - j] * h[j];
+        }
+        y[i] /= 7.0; // 7 = 1+2+4
+    }
+}
+
+int main(void)
+{
+    float in[100];
+    float out[100];
+
+    apply(in, out, 100);
+    
+    return 0;
+}
