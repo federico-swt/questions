3dbb2c5ca96523c142ac78feed38338edfd8257e
federico-swt <federico.sossai@stockholmwater.com>
2022-07-01 15:50:49 +0200

    Update c03

--- c03.c
+++ c03.c
@@ -1,16 +1,24 @@
-float *three_numbers()
+float *four_numbers()
 {
-    float n[3] = { 0.1, 2.3, 4.5 };
-    return n;
+    float x[4];
+    int i = 0;
+
+    x[++i] = 0.1;
+    x[++i] = 2.3;
+    x[++i] = 4.5;
+    x[++i] = 6.7;
+
+    return x;
 }
 
 int main()
 {
-    float *p = three_numbers();
+    float *p = four_numbers();
 
     printf("%i\n", p[0]);
     printf("%i\n", p[1]);
     printf("%i\n", p[2]);
+    printf("%i\n", p[3]);
 
     return 0;
 }
